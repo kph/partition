6d7fe03ef3beed2a9b39842eca96826d4cd3a8ac
Kevin Paul Herbert <kph@platinasystems.com>
2020-04-03 16:08:05 -0600

    partition: factor more

--- partition.go
+++ partition.go
@@ -6,10 +6,8 @@
 package partition
 
 import (
-	"encoding/binary"
 	"errors"
 	"fmt"
-	"io"
 	"os"
 )
 
@@ -37,49 +35,6 @@ func (e PartitionError) Is(target error) bool {
 
 func (e PartitionError) Unwrap() error {
 	return e.wrapped
-}
-
-func (t *PartitionTable) ParseBootRecord(f io.ReadSeeker, dev string, base int64) (err error) {
-	p, err := f.Seek(base, io.SeekStart)
-	if err != nil {
-		return &PartitionError{ErrSeekingDev, err,
-			fmt.Sprintf("%v %s offset %d: %v",
-				ErrSeekingDev, dev, base, err)}
-	}
-	if p != base {
-		return &PartitionError{ErrUnexpectedSeek, nil,
-			fmt.Sprintf("%v %s offset %d seeked to %d instead",
-				ErrUnexpectedSeek, dev, base, p)}
-	}
-	br := BootRecord{}
-	err = binary.Read(f, binary.LittleEndian, &br)
-	if err != nil {
-		return &PartitionError{ErrReadingDev, err,
-			fmt.Sprintf("%v %s offset %d: %v",
-				ErrReadingDev, dev, base, err)}
-	}
-	//fmt.Println(br.String())
-
-	for i := 0; i < 4; i++ {
-		if br.Partitions[i].IsUsed() && !br.Partitions[i].IsExtended() {
-			t.Table = append(t.Table, br.Partitions[i])
-		} else {
-			if base == 0 {
-				t.Table = append(t.Table, PartitionEntry{})
-			}
-		}
-	}
-
-	for i := 0; i < 4; i++ {
-		if br.Partitions[i].IsExtended() {
-			offset := base + (int64(br.Partitions[i].Lba) * 512)
-			err = t.ParseBootRecord(f, dev, offset)
-			if err != nil {
-				return err
-			}
-		}
-	}
-	return nil
 }
 
 func Analyze(dev string) (err error) {
